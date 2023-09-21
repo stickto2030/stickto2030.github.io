95e1d77361ba87d3216243bba2cff85bb4d7abb6
Stick To 2030 <webmaster@stickto2030.co.uk>
2023-09-21 20:02:10 +0100

    Fix js.

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 'use strict';
 window.onload = function () {
-    update = document.getElementById("update");
+    const update = document.getElementById("update");
     const e = update.innerText.replace(' AT ', String.fromCharCode(64)).replaceAll(' DOT ', String.fromCharCode(46));
     update.innerText = e;
     update.href = 'mailto:' + e;
